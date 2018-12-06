52f37d004d0f9f587d813d18ee25e03924bd61ba
Marius Flagstad <marius.flagstad@outlook.com>
2018-12-06 14:29:46 +0100

    Relative path

--- public/js/aframe-ar-init.js
+++ public/js/aframe-ar-init.js
@@ -2,18 +2,18 @@ window.aframeInitObject = [
     {
         preset: "marius",
         type: "pattern",
-        patternUrl: "http://localhost:1337/marker/test.patt",
+        patternUrl: "/marker/test.patt",
         markersAreaEnabled: false
     },{
         preset: "test",
         type: "pattern",
-        patternUrl: "http://localhost:1337/markers/patt/familiebonus.patt",
+        patternUrl: "/markers/patt/familiebonus.patt",
         markersAreaEnabled: false
     },
     {
         preset: "merblack",
         type: "pattern",
-        patternUrl: "http://localhost:1337/marker/merblack.patt",
+        patternUrl: "/marker/merblack.patt",
         markersAreaEnabled: false
 
     }
