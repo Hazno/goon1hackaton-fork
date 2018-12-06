f552b922a8a222621aba978b9ac3172fc75e278c
Hanne Mathisen <hma@itverket.no>
2018-12-06 16:44:29 +0100

    update click event for familiebonus

--- public/js/events.js
+++ public/js/events.js
@@ -5,11 +5,7 @@ AFRAME.registerComponent('markerhandler', {
 
         familiebonusMarker.addEventListener('click', function(ev){
             if (familiebonusMarker.object3D.visible == true && ev.detail.cursorEl) {
-                const entity = document.querySelector('#box-model');
-                const scale = entity.getAttribute('scale');
-                Object.keys(scale).forEach((key) => scale[key] = scale[key] + 0.5);
-
-                entity.setAttribute('scale', scale);
+                window.location.href = "https://telenor.no";
             }
         });
     }});
