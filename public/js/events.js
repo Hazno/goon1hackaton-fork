cf8e976b568a6643e09d2baebd8410df8ab44982
René Hagen Hage <rhh@develop.no>
2018-12-06 16:41:48 +0100

    Camera fix? Also added click debugging for multiple boxes.

--- public/js/events.js
+++ public/js/events.js
@@ -1,15 +1,23 @@
 AFRAME.registerComponent('markerhandler', {
+	init: function () {
+		const familiebonusMarker = document.querySelector("#familiebonus-marker");
 
-    init: function() {
-        const familiebonusMarker = document.querySelector("#familiebonus-marker");
+		familiebonusMarker.addEventListener('click', function (ev) {
+			if (familiebonusMarker.object3D.visible === true && ev.detail.cursorEl) {
+				const entity = familiebonusMarker.querySelector('#box-model');
+				const scale = entity.getAttribute('scale');
+				Object.keys(scale).forEach((key) => scale[key] = scale[key] + 0.5);
 
-        familiebonusMarker.addEventListener('click', function(ev){
-            if (familiebonusMarker.object3D.visible == true && ev.detail.cursorEl) {
-                const entity = document.querySelector('#box-model');
-                const scale = entity.getAttribute('scale');
-                Object.keys(scale).forEach((key) => scale[key] = scale[key] + 0.5);
+				entity.setAttribute('scale', scale);
+			}
+		});
+	}
+});
 
-                entity.setAttribute('scale', scale);
-            }
-        });
-    }});
+AFRAME.registerComponent('product-picker', {
+	init: function() {
+		this.el.addEventListener('click', function(e) {
+			console.log('CLICK', e);
+		});
+	}
+});
