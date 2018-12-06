66fae2a11b8c04acbb35d7777882c16bb1d728bd
Marius Flagstad <marius.flagstad@outlook.com>
2018-12-06 14:02:57 +0100

    Custom init

---
+++ public/js/aframe-ar-init.js
@@ -0,0 +1,13 @@
+window.aframeInitObject = [
+    {
+        preset: "marius",
+        type: "pattern",
+        patternUrl: "http://localhost:1337/marker/test.patt",
+        markersAreaEnabled: false
+    },{
+        preset: "test",
+        type: "pattern",
+        patternUrl: "http://localhost:1337/marker/pattern-marker.patt",
+        markersAreaEnabled: false
+    }
+];
