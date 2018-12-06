83bf21618c3d8e8a949740ffe1e512d822326727
René Hagen Hage <rhh@develop.no>
2018-12-06 15:13:04 +0100

    Added multiple boxes for telenor-logo marker.

--- public/js/aframe-ar-init.js
+++ public/js/aframe-ar-init.js
@@ -9,12 +9,15 @@ window.aframeInitObject = [
         type: "pattern",
         patternUrl: "/markers/patt/familiebonus.patt",
         markersAreaEnabled: false
-    },
-    {
+    },{
         preset: "merblack",
         type: "pattern",
         patternUrl: "/markers/patt/merblack.patt",
         markersAreaEnabled: false
-
-    }
+    },{
+		preset: "logo",
+		type: "pattern",
+		patternUrl: "/markers/patt/telenor-logo.patt",
+		markersAreaEnabled: false
+	}
 ];
